74faa2048326d2ef97f5bc5b965891d58031ac83
lukechaston@gmail.com <lukechaston@Lukes-MacBook-Pro.local>
2020-02-13 21:55:32 +0000

    An exercise that tests leap years.

---
+++ LeapYear.cc
@@ -0,0 +1,19 @@
+int main() {
+  input x;
+  std::cout << "What is the year?";
+  std::cin >> x;
+  
+  if (x % 4 == 0) {
+       std::cout << "This is a leap year.";
+   }
+   else if (x % 100 == 0) {
+     std::cout << "This is a leap year."
+   }
+   else if (x % 400 == 0) {
+     std::cout << "This is a leap year."
+   }
+  else {
+        std::cout << "This is not a leap year";
+ }
+  
+}
